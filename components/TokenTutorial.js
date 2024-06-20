ee0971f6a746526d6156f92b7d571a2c50f797fd
vipgio <e.nabavi78@gmail.com>
2024-06-20 02:52:52 +0200

    new login, direct owned2

---
+++ components/TokenTutorial.js
@@ -0,0 +1,84 @@
+import { Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { toast } from "react-toastify";
+import ImageWrapper from "HOC/ImageWrapper";
+
+const TokenTutorial = ({ showModal, setShowModal }) => {
+	const handleCopyClick = async () => {
+		try {
+			await navigator.clipboard.writeText(
+				`const info = {jwt: localStorage.getItem("new:jwt:token"), expires: localStorage.getItem("new:jwt:expires")};console.log(info);`
+			);
+			toast.success(`Code copied!`, {
+				toastId: "copy",
+				autoClose: 2000,
+				hideProgressBar: false,
+			});
+		} catch (err) {
+			console.log(err);
+			toast.error(`Failed to copy the code :(`, { toastId: "failed" });
+			toast.error(err.response.data.error, {
+				toastId: err.response.data.errorCode,
+			});
+		}
+	};
+	return (
+		<Transition
+			show={showModal}
+			enter='transition-opacity duration-75'
+			enterFrom='opacity-0'
+			enterTo='opacity-100'
+			leave='transition-opacity duration-150'
+			leaveFrom='opacity-100'
+			leaveTo='opacity-0'
+			as={Fragment}
+		>
+			<Dialog open={showModal} onClose={() => setShowModal(false)}>
+				<div className='fixed inset-0 bg-black/80' aria-hidden='true' />
+
+				{/* Full-screen container to center the panel */}
+				<div className='fixed inset-0 flex w-screen items-center justify-center p-4'>
+					{/* The actual dialog panel  */}
+					<Dialog.Panel className='mx-3 rounded bg-white p-3'>
+						<div className='inline-flex w-full flex-col md:flex-row'>
+							<ol className='list-inside list-decimal md:w-1/2'>
+								<li>Login to your Kolex account from the official website</li>
+								<li>Open Developer Console (usually F12) and navigate to the Console tab.</li>
+								<li>
+									Paste{" "}
+									<span className='cursor-pointer underline' onClick={handleCopyClick}>
+										this
+									</span>{" "}
+									code in the console and hit enter.
+								</li>
+								<p>
+									(If you see the warning about pasting codes into console, type &quot;allow pasting&quot; and
+									try again.)
+								</p>
+								<li>Right click on the output and hit &quot;Copy object&quot;.</li>
+								<li>Paste the text in the field below.</li>
+							</ol>
+							<div className='relative md:w-1/2'>
+								<ImageWrapper
+									src={"https://i.imgur.com/6y8Mf4I.png"}
+									fill
+									alt={"Example"}
+									width={500}
+									height={500}
+								/>
+							</div>
+						</div>
+						<p className='mt-4'>
+							<span className='font-bold text-red-400 underline'>NEVER</span> paste anything in the console if
+							you don&apos;t know or trust the person sending you the code or what the code does.
+						</p>
+						<p className='mt-2 text-sm'>
+							It&apos;s not possible to use a phone to use the site until Kolex fixes it.
+						</p>
+					</Dialog.Panel>
+				</div>
+			</Dialog>
+		</Transition>
+	);
+};
+export default TokenTutorial;
--- pages/login.js
+++ pages/login.js
@@ -1,16 +1,41 @@
 import { useContext, useState } from "react";
-import ReCAPTCHA from "react-google-recaptcha";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaDiscord } from "react-icons/fa";
 import { UserContext } from "context/UserContext";
-import Tooltip from "@/components/Tooltip";
 import "react-toastify/dist/ReactToastify.css";
-import LoadingSpin from "@/components/LoadingSpin";
+import JWT from "@/components/login/JWT";
+import TokenTutorial from "@/components/TokenTutorial";
+import Link from "next/link";
 
 const Login = () => {
-	const { setUser, loading, setLoading } = useContext(UserContext);
-	const [jwt, setJWT] = useState("");
+	const { setUser } = useContext(UserContext);
+	const [loading, setLoading] = useState(false);
+	// const [email, setEmail] = useState("");
+	// const [password, setPassword] = useState("");
+	// const [code, setCode] = useState("");
+	const [jwt, setJwt] = useState("");
+	const [codeEnabled, setCodeEnabled] = useState(false);
+	const [showTutorial, setShowTutorial] = useState(false);
+
+	const handleCopyClick = async () => {
+		try {
+			await navigator.clipboard.writeText(
+				`const info = {jwt: localStorage.getItem("new:jwt:token"), expires: localStorage.getItem("new:jwt:expires")};console.log(info);`
+			);
+			toast.success(`Code copied!`, {
+				toastId: "copy",
+				autoClose: 2000,
+				hideProgressBar: false,
+			});
+		} catch (err) {
+			console.log(err);
+			toast.error(`Failed to copy the code :(`, { toastId: "failed" });
+			toast.error(err.response.data.error, {
+				toastId: err.response.data.errorCode,
+			});
+		}
+	};
 
 	const onSubmit = async (e) => {
 		e.preventDefault();
@@ -18,16 +43,17 @@ const Login = () => {
 		try {
 			const { data } = await axios.get("/api/info", {
 				headers: {
-					jwt: jwt,
+					jwt: JSON.parse(jwt).jwt,
 				},
 			});
+			console.log(data);
 			setUser({
 				user: {
 					...data.data,
 				},
-				jwt: jwt,
+				expires: JSON.parse(jwt).expires,
+				jwt: JSON.parse(jwt).jwt,
 				info: { allowed: ["history"] },
-				expires: Date.now() / 1000 + 5400,
 			});
 			setLoading(false);
 		} catch (err) {
@@ -51,62 +77,65 @@ const Login = () => {
 				draggable
 				pauseOnHover
 			/>
-			{/* <div className='flex w-full'> */}
-			<div className='absolute right-0 top-4' title='Source Code'>
-				<a
-					href='https://github.com/vipgio/hro-vip'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
+			<div className='text-primary-500 mt-16 flex h-1/4 w-full flex-col items-center justify-center lg:mt-11'>
+				<p className='text-center'>
+					<span className='block cursor-pointer underline' onClick={handleCopyClick}>
+						Click to copy the code if you already know how it works
+					</span>{" "}
+				</p>
+				<button className='simple-button mt-1' onClick={() => setShowTutorial(true)}>
+					HOW
+				</button>
+			</div>
+
+			<div
+				className='absolute right-0 top-4 flex h-12 items-center justify-center rounded-b-md font-semibold text-gray-700 transition-colors dark:text-gray-300'
+				title='Source Code'
+			>
+				<a href='https://github.com/vipgio/kolex-vip' target='_blank' rel='noopener noreferrer'>
 					<FaGithub className='h-6 w-6 text-gray-700 hover:text-gray-600 dark:text-gray-200 dark:hover:text-gray-300 dark:active:text-gray-400' />
 				</a>
-			</div>
-			<div className='flex h-full w-full flex-col items-center justify-center'>
-				<span className='pb-2 text-xl font-semibold text-gray-700 dark:text-gray-300'>
-					Login using your Hro account
-				</span>
-				{/* <ReCAPTCHA sitekey={"0da71416-b1e2-480c-a16a-405a41e787a1"} /> */}
-				<form
-					className='flex flex-col items-center space-y-2 rounded-md border border-gray-700 p-2 dark:border-gray-300'
-					onSubmit={onSubmit}
+				<a
+					href='https://discordapp.com/users/473436055958192128'
+					target='_blank'
+					rel='noreferrer'
+					title='Contact me on Discord'
+					// className='rounded-full focus:outline-primary-500 focus-visible:outline-offset-4 focus-visible:outline-primary-500'
+					className='my-outline mx-3 h-8 w-8 rounded-full focus-visible:ring-inset'
 				>
-					<input
-						type='text'
-						name='jwt'
-						autoComplete='off'
-						placeholder='JWT'
-						value={jwt}
-						onChange={(e) => setJWT(e.target.value)}
-						disabled={loading}
-						className={`input-field ${loading ? "cursor-not-allowed opacity-50" : ""}`}
-						autoFocus
-					/>
+					<FaDiscord className='h-full w-full hover:text-gray-600 dark:hover:text-gray-200' />
+				</a>
+				<Link href='/features'>
+					<a className='my-outline text-primary-500 dark:bg-primary-500 dark:hover:bg-primary-600 dark:active:bg-primary-700 mr-2 rounded bg-gray-100  p-1.5 transition-colors hover:bg-gray-200 focus-visible:ring-offset-1 active:bg-gray-300 dark:text-gray-100'>
+						Features
+					</a>
+				</Link>
+			</div>
+			<div>
+				<TokenTutorial showModal={showTutorial} setShowModal={setShowTutorial} />
+			</div>
 
-					<button type='submit' disabled={loading} className='submit-button'>
-						{loading ? <LoadingSpin /> : "Login"}
-					</button>
-					<div className='pr-2 text-gray-700 dark:text-gray-300'>
-						<Tooltip
-							text={
-								"Your password is never stored or sent to anywhere other than hro. If you have any questions you can check the source code or contact me on discord vipgio#4884"
-							}
-							direction='right'
-						/>
-					</div>
-				</form>
-				<div className='absolute bottom-0 h-8 w-full border-t border-gray-600 p-1 dark:border-gray-400'>
-					<span className='text-sm text-gray-700 dark:text-gray-300'>
-						Cool site logo by{" "}
-						<a
-							href='https://discordapp.com/users/341825527104929792'
-							target='_blank'
-							rel='noreferrer'
-							className='text-main-500 hover:underline'
-						>
-							Nexus
-						</a>
-					</span>
-				</div>
+			<JWT
+				jwt={jwt}
+				loading={loading}
+				setJwt={setJwt}
+				toast={toast}
+				setLoading={setLoading}
+				setUser={setUser}
+				onSubmit={onSubmit}
+			/>
+			<div className='absolute bottom-0 h-8 w-full border-t border-gray-600 p-1 dark:border-gray-400'>
+				<span className='text-sm text-gray-700 dark:text-gray-300'>
+					Cool site logo by{" "}
+					<a
+						href='https://discordapp.com/users/341825527104929792'
+						target='_blank'
+						rel='noreferrer'
+						className='text-primary-500 hover:underline'
+					>
+						Nexus
+					</a>
+				</span>
 			</div>
 		</>
 	);
